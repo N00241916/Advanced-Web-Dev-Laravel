d548244004602d997f8aa975f2f79e1b8abfa4c9
Isabelle Ryan (Student) <N00241916@iadt.ie>
2025-09-29 15:02:23 +0100

    Project started

---
+++ 2025-vocalsynths-app/app/Http/Controllers/VSynthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VSynth;
+use Illuminate\Http\Request;
+
+class VSynthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VSynth $vSynth)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VSynth $vSynth)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, VSynth $vSynth)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VSynth $vSynth)
+    {
+        //
+    }
+}
